8c5cc4ac88b7640a6d819e2a839253a09746c95f
hishamsammyy <71552945+hishamsammyy@users.noreply.github.com>
2020-11-03 22:24:41 +0200

    Update main_activity.java

--- main_activity.java
+++ main_activity.java
@@ -19,11 +19,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Spinner spinnerPbase,spinnerTbase,spinnerP1,spinnerP2,spinnerT,spinnerD,spinnerL,spinnerH2H1,spinnerVi;
 // define string variables to store the selected value of spinner
     String textPbase,textTbase,textP1,textP2,textT,textD,textL,textH2H1,textVi;
+//define glabal edit text variables to store the entered data from the edit text so that it could be accessed from any where
+    EditText editTextPbase,editTextTbase,editTextP1,editTextP2,editTextT,editTextD,editTextL,editTextE,editTextG,editTextH2H1,editTextZ,editTextVi;
+
+    TextView textViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //intialize the spinner variable with the spinner values(array value)
         setContentView(R.layout.activity_main);
+        //linking all spinner variables with their corresponding spinner elements in the main activity
         spinnerPbase= (Spinner) findViewById(R.id.spinnerPbase);
         spinnerPbase.setOnItemSelectedListener(this);
         spinnerTbase= (Spinner) findViewById(R.id.spinnerTbase);
@@ -42,6 +46,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinnerH2H1.setOnItemSelectedListener(this);
         spinnerVi= (Spinner) findViewById(R.id.spinnerVi);
         spinnerVi.setOnItemSelectedListener(this);
+        //linking all edit text variables with their corresponding edit text elements in the main activity
+        editTextPbase = (EditText) findViewById(R.id.editTextPbase);
+        editTextTbase = (EditText) findViewById(R.id.editTextTbase);
+        editTextP1 = (EditText) findViewById(R.id.editTextP1);
+        editTextP2 = (EditText) findViewById(R.id.editTextP2);
+        editTextT = (EditText) findViewById(R.id.editTextT);
+        editTextD = (EditText) findViewById(R.id.editTextD);
+        editTextL = (EditText) findViewById(R.id.editTextL);
+        editTextE = (EditText) findViewById(R.id.editTextE);
+        editTextG = (EditText) findViewById(R.id.editTextG);
+        editTextH2H1 = (EditText) findViewById(R.id.editTextH2H1);
+        editTextZ = (EditText) findViewById(R.id.editTextZ);
+        editTextVi = (EditText) findViewById(R.id.editTextVi);
+        textViewResult =(TextView) findViewById(R.id.textViewResult);
     }
 
     @Override
@@ -78,24 +96,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             textVi = parent.getItemAtPosition(position).toString();
         }
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
     Double Pbase,Tbase,P1,P2,Tavg,Dpipe,Lpipe,Epipe,Zgas,Ggas,H2H1,Vi,Spipe,Lepipe;
     Double  panhandleA,panhandleB,waymouth,IGT;
-    double panhandleAroundOff;
     @SuppressLint("DefaultLocale")
     public void calculate(View view) {
-        EditText editTextPbase = (EditText) findViewById(R.id.editTextPbase);
-        EditText editTextTbase = (EditText) findViewById(R.id.editTextTbase);
-        EditText editTextP1 = (EditText) findViewById(R.id.editTextP1);
-        EditText editTextP2 = (EditText) findViewById(R.id.editTextP2);
-        EditText editTextT = (EditText) findViewById(R.id.editTextT);
-        EditText editTextD = (EditText) findViewById(R.id.editTextD);
-        EditText editTextL = (EditText) findViewById(R.id.editTextL);
-        EditText editTextE = (EditText) findViewById(R.id.editTextE);
-        EditText editTextG = (EditText) findViewById(R.id.editTextG);
-        EditText editTextH2H1 = (EditText) findViewById(R.id.editTextH2H1);
-        EditText editTextZ = (EditText) findViewById(R.id.editTextZ);
-        EditText editTextVi = (EditText) findViewById(R.id.editTextVi);
-        TextView textViewResult =(TextView) findViewById(R.id.textViewResult);
 
         if (editTextPbase.getText().toString().isEmpty()) {
             Toast.makeText(MainActivity.this, "please enter base pressure value", Toast.LENGTH_SHORT).show();
@@ -141,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             } else if (textPbase.equals("Psi")) {
                 Pbase = Double.parseDouble(editTextPbase.getText().toString()) * 6.89475729;
             }
-           if (textTbase.equals("C")) {
+            if (textTbase.equals("C")) {
                 Tbase = Double.parseDouble(editTextTbase.getText().toString()) +273.15;
             } else if (textTbase.equals("K")) {
                 Tbase = Double.parseDouble(editTextTbase.getText().toString());
@@ -181,13 +191,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 Dpipe = Double.parseDouble(editTextD.getText().toString());
             }
             if (textL.equals("Km")) {
-               Lpipe = Double.parseDouble(editTextL.getText().toString()) ;
+                Lpipe = Double.parseDouble(editTextL.getText().toString()) ;
             } else if (textL.equals("Miles")) {
                 Lpipe = Double.parseDouble(editTextL.getText().toString())*1.609344;
             } else if (textL.equals("M")) {
                 Lpipe = Double.parseDouble(editTextL.getText().toString()) /1000;
             } else if (textL.equals("Feet")) {
-            Lpipe = Double.parseDouble(editTextL.getText().toString()) *0.3048/1000;
+                Lpipe = Double.parseDouble(editTextL.getText().toString()) *0.3048/1000;
             }
             if (textH2H1.equals("Km")) {
                 H2H1 = Double.parseDouble(editTextH2H1.getText().toString())*1000 ;
@@ -219,19 +229,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             panhandleA=4.5965/1000*Epipe*(Math.pow((Tbase/Pbase),1.0788))*(Math.pow((Math.pow(P1,2)-Math.pow(2.718,Spipe)*Math.pow(P2,2))/(Math.pow(Ggas,0.8539)*Tavg*Lepipe*Zgas),0.5394))*Math.pow(Dpipe,2.6182);
 
             DecimalFormat f = new DecimalFormat("##.00");
-            panhandleAroundOff =  (double) Math.round(panhandleA * 100) / 100;
 
 
             Toast.makeText(MainActivity.this, panhandleA.toString(), Toast.LENGTH_SHORT).show();
             textViewResult.setText(f.format(panhandleA));
-            editTextPbase.requestFocus();
 
         }
     }
-
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    public void onClickReset(View view) {
+        editTextP1.setText("");
+        editTextP2.setText("");
+        editTextT.setText("");
+        editTextD.setText("");
+        editTextL.setText("");
+        editTextE.setText("");
+        editTextG.setText("");
+        editTextZ.setText("");
+        textViewResult.setText("");
+       editTextPbase.requestFocus();
     }
+
 }
